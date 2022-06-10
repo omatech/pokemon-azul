22f6dc2ba391ab1d6e8971ef7ac8085b702146fe
ecasas91 <105354863+ecasas91@users.noreply.github.com>
2022-06-10 13:54:53 +0200

    message no pokemonList

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import List from "./List";
+import ElementList from "./List/ElementList";
+import pokemons from "/public/data/pokemons.json"
+
+const App = () => {
+    const pokemonsList = pokemons.filter((pokemon) =>
+      pokemon.types.find((type) => type === "Fuego")
+    );
+    console.log(pokemonsList);
+    return !pokemonsList.length ? (
+      "No Pokemon"
+    ) : (
+      <List>
+        <ElementList pokemons={pokemonsList} />
+      </List>
+    );
+  };
+
+export default App;
